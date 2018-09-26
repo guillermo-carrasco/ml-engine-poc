0bc15f3698cd701ca100ebfe9d47f56066ed5e3c
Guillermo Carrasco <guille.ch.88@gmail.com>
2018-09-26 11:38:27 -0400

    Use pickle instead of joblib

--- predict.sh
+++ predict.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-gcloud ml-engine predict --model twitter_sentiment --text-instances data/test_small.csv --version v1
+
+run_local="local"
+if [ "$1" == "$run_local" ]; then
+    gcloud ml-engine local predict --model-dir=`pwd` --text-instances data/test_small.csv --framework SCIKIT_LEARN
+else
+    gcloud ml-engine predict --model twitter_sentiment --text-instances data/test_small.csv --version v1
+fi
--- rule_based/rule.py
+++ rule_based/rule.py
@@ -1,25 +1,33 @@
 import datetime
 import os
+import pickle
 import subprocess
 import sys
 
-from sklearn.externals import joblib
+
+from sklearn.base import BaseEstimator
 
 
 class Model():
     def predict(self, samples):
         """
-        Rule based model that predicts READER if the word READER is present in the string, else OTHER
-        :param s: Array of strings
-        :return:
+        Rule based model: predicts class READER if the word READER is present in the string, else predicts class OTHER
+        :param samples: Array of text strings
+        :return: Array of predictions
         """
         return ['READER' if s.lower().count('reader') else 'OTHER' for s in samples]
 
 
-model_name = 'model.joblib'
+model_name = 'model.pkl'
 model = Model()
-joblib.dump(model, model_name)
+Model.__module__ = 'rule'
+
+# Export the classifier to a file
+with open('model.pkl', 'wb') as model_file:
+  pickle.dump(model, model_file)
 
 # Upload the saved model file to Cloud Storage
-model_path = os.path.join('gs://', 'guillermo-ml-test', datetime.datetime.now().strftime('rule_%Y%m%d_%H%M%S'), model_name)
+model_path = os.path.join('gs://', 'guillermo-ml-test',
+                          datetime.datetime.now().strftime('rule_%Y%m%d_%H%M%S'),
+                          model_name)
 subprocess.check_call(['gsutil', 'cp', model_name, model_path], stderr=sys.stdout)
